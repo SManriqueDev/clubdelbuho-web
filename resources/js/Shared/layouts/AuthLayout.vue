1e0042414f830b60e6d599e98a76fd5eb7bafc6b
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2020-11-21 11:44:54 -0500

    feat: auth layout

---
+++ resources/js/Shared/layouts/AuthLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <slot></slot>
+    <footer class="py-2 px-2">
+      Mundo Lector - Desarrollado por Alen Software &copy;
+      {{ new Date().getFullYear() }}
+    </footer>
+  </div>
+</template>
