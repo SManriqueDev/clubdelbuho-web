fa3835c258c69a3ebf9516852679414f03a82fcc
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2020-11-24 23:02:04 -0500

    feat: strengths card with svgs assets

---
+++ resources/js/components/StrengthsCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="rounded-lg border-2 border-purple-600 px-6 py-6">
+        <p class="text-lg font-bold mb-3">
+            Fortalezas practicas
+        </p>
+        <div class="mb-3">
+            <strong>
+                Nivel de lectura:
+            </strong>
+            <ol>
+                <li>
+                    Literal
+                </li>
+                <li>
+                    Crítico
+                </li>
+            </ol>
+        </div>
+        <div class="mb-3">
+            <strong>
+                Tipos de texto
+            </strong>
+            <ol>
+                <li>
+                    Narrativo continuo
+                </li>
+                <li>
+                    Informativo discontinuo
+                </li>
+            </ol>
+        </div>
+        <div class="mb-3">
+            <strong>
+                Área de conocimiento
+            </strong>
+            <ol>
+                <li>
+                    Literatura
+                </li>
+                <li>
+                    Ciencias sociales
+                </li>
+            </ol>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- resources/js/pages/Student/Index.vue
+++ resources/js/pages/Student/Index.vue
@@ -1,19 +1,51 @@
 <template>
-  <div>
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-3">
-      <div>Mi Avatar</div>
-      <div>Fondo personalizado</div>
-      <div>Mis últimas recompensas</div>
+    <div>
+        <!-- Student Actions -->
+        <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-12">
+            <!-- Avatar -->
+            <div class="text-center mx-auto">
+                <AvatarSvg />
+                <span class="font-bold text-lg">Mi Avatar</span>
+            </div>
+            <div class="text-center mx-auto">
+                <!-- Background -->
+                <MobileSvg />
+                <span class="text-lg font-bold">
+                    Fondo personalizado
+                </span>
+            </div>
+            <div class="text-center mx-auto">
+                <WinnersSvg />
+
+                <span class="text-lg font-bold">
+                    Mis últimas recompensas
+                </span>
+            </div>
+        </div>
+
+        <!-- Resume Student -->
+
+        <div class="grid grid-cols-2 gap-4 w-10/12 mx-auto">
+            <StrengthsCard v-for="n in 4" :key="n" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import StudentLayout from "@/shared/layouts/StudentLayout";
+import StrengthsCard from "@/components/StrengthsCard";
+import MobileSvg from "@/components/SVGs/MobileSvg";
+import AvatarSvg from "@/components/SVGs/AvatarSvg";
+import WinnersSvg from "@/components/SVGs/WinnersSvg";
 export default {
-  layout: StudentLayout,
+    layout: StudentLayout,
+    components: {
+        StrengthsCard,
+        AvatarSvg,
+        WinnersSvg,
+        MobileSvg
+    }
 };
 </script>
 
-<style>
-</style>
+<style></style>
