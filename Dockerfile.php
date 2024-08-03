5dd2763491802f6ff7bf733b581ca9c534cc10e5
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 16:45:19 -0500

    fix: composer vendor files

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,19 +1,19 @@
-FROM composer as builder
-WORKDIR /var/www/html
-COPY composer.* ./
-RUN composer install
-
 FROM php:7.4-fpm-alpine
+
 # Set working directory
 WORKDIR /var/www/html
 
 # Install PHP extensions
 RUN docker-php-ext-install pdo pdo_mysql
 
-# Copy existing application directory permissions
-COPY --from=builder /var/www/html/vendor /var/www/html/vendor
+# Install Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Install Composer dependencies
+RUN composer install --no-autoloader 
 
 # Copy existing application directory contents
 COPY . .
 
-
+# Generate autoloader
+RUN composer dump-autoload -o
