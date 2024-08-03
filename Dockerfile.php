73e52bfd519aced9a949c9cb07aac679ede00a98
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 16:01:01 -0500

    fix: updated dockerfile

---
+++ Dockerfile.php
@@ -0,0 +1,16 @@
+FROM php:7.4-fpm-alpine
+
+# Set working directory
+WORKDIR /var/www/html
+
+# Install PHP extensions
+RUN docker-php-ext-install pdo pdo_mysql
+
+# Install Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Copy existing application directory contents
+COPY . .
+
+# Install Composer dependencies
+RUN composer install
