6228b9a0eb61c06801400d1170761e784e741d90
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 17:57:52 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,24 +1,25 @@
+
 FROM php:7.4-fpm-alpine
 
-
-WORKDIR /var/www/html
-
-RUN docker-php-ext-install pdo pdo_mysql
-
 ENV COMPOSER_ALLOW_SUPERUSER=1
-
+ENV PHP_UID=1000
+ENV PHP_GID=1000
 
 
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+RUN docker-php-ext-install pdo pdo_mysql
+RUN mkdir -p /var/www/html/storage/framework/{sessions,views,cache} \
+    && mkdir -p /var/www/html/bootstrap/cache \
+    && mkdir -p /var/www/html/storage/logs \
+    && addgroup -g ${PHP_UID} www \
+    && adduser -H -D -u ${PHP_GID} -G www www \
+    && chown -R www:www /var/www/html
+
+WORKDIR /var/www/html
+USER www
 
 COPY ./composer.json ./composer.lock ./
 COPY . .
 
 RUN composer install --no-interaction --no-dev --prefer-dist --no-scripts --no-progress
-
-COPY . .
-
 RUN composer dump-autoload --optimize
-
-RUN chown -R www-data:www-data storage && \
-    chmod -R 775 storage
