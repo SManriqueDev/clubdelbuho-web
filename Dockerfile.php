df3c99ccaa9787ca26e2382177cb7cfcca339e75
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 16:32:04 -0500

    fix: updated dockerfile

--- Dockerfile.php
+++ Dockerfile.php
@@ -6,20 +6,13 @@ WORKDIR /var/www/html
 # Install PHP extensions
 RUN docker-php-ext-install pdo pdo_mysql
 
-# Install Composer
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
-
 # Copy existing application directory contents
 COPY . .
 
+# Install Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
 # Install Composer dependencies
-RUN composer install
+RUN composer install --no-autoloader 
 
 RUN composer dump-autoload --optimize
-
-
-# Expose port 9000 for the PHP-FPM server
-EXPOSE 8000
-
-# Run the application
-CMD ["php", "artisan", "serve", "--host=0.0.0.0", "--port=8000"]
