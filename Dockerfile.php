1c64be2508ebdf4ffe99c61a83fa55e862a21a1e
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 18:09:29 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -5,7 +5,6 @@ ENV COMPOSER_ALLOW_SUPERUSER=1
 ENV PHP_UID=1000
 ENV PHP_GID=1000
 
-
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 RUN docker-php-ext-install pdo pdo_mysql
 RUN mkdir -p /var/www/html/storage/framework/{sessions,views,cache} \
@@ -13,7 +12,8 @@ RUN mkdir -p /var/www/html/storage/framework/{sessions,views,cache} \
     && mkdir -p /var/www/html/storage/logs \
     && addgroup -g ${PHP_UID} www \
     && adduser -H -D -u ${PHP_GID} -G www www \
-    && chown -R www:www /var/www/html
+    && chown -R www:www /var/www/html \
+    && chmod -R 775 /var/www/html/storage
 
 WORKDIR /var/www/html
 USER www
@@ -21,3 +21,4 @@ USER www
 COPY . .
 
 RUN composer install --no-interaction --no-dev --prefer-dist --no-scripts --no-progress
+RUN composer dump-autoload --optimize
