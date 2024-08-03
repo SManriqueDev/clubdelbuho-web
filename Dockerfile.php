189b534f765168cb6eb4bf8d0cf9011440bf6e6a
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 17:46:30 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,13 +1,5 @@
-FROM composer:2.5 as composer
-
-WORKDIR /var/www/html
-
-COPY ./composer.json ./composer.lock ./
-# Copy the database folder content before running composer install
-COPY ./database /var/www/html/database
-RUN composer install --no-interaction --no-dev --prefer-dist --no-scripts --no-progress
-
 FROM php:7.4-fpm-alpine
+
 
 WORKDIR /var/www/html
 
@@ -15,11 +7,13 @@ RUN docker-php-ext-install pdo pdo_mysql
 
 ENV COMPOSER_ALLOW_SUPERUSER=1
 
-COPY --from=composer /var/www/html/vendor/ /var/www/html/vendor/
-COPY --chown=www-data:www-data . /var/www/html
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
+COPY ./composer.json ./composer.lock ./
+COPY . .
 
-RUN chown -R www-data:www-data /var/www/html/storage && \
-    chmod -R 775 /var/www/html/storage
+RUN composer install --no-interaction --no-dev --prefer-dist --no-scripts --no-progress
+
+COPY . .
 
 RUN composer dump-autoload --optimize
