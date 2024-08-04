48c89decf78d7d7fb988ee2fca64e5ea18980e6e
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 19:16:15 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -6,13 +6,15 @@ RUN addgroup -g 1000 laravel && adduser -G laravel -g laravel -s /bin/sh -D lara
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
 RUN mkdir -p /var/www/html
-RUN chown laravel:laravel /var/www/html
+RUN chown -R laravel:laravel /var/www/html
 
 # Set working directory
 WORKDIR /var/www/html
 
 COPY . .
 RUN docker-php-ext-install pdo pdo_mysql
+
+USER laravel
 
 # Expose port 9000 and start php-fpm server
 EXPOSE 9000
