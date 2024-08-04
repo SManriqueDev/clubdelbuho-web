8294ae5769c1993343f4482b7b52070c5f9b3198
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 19:02:20 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -38,6 +38,7 @@ RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local
 # Copy existing application directory contents
 COPY . /var/www/html
 
+RUN chown -R www-data:www-data /var/www/html
 # Expose port 9000 and start php-fpm server
 EXPOSE 9000
 CMD ["php-fpm"]
