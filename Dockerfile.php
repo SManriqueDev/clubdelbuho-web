1394b7e11f78cedc5d6128d14510fe8338d30faf
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-04 14:42:13 -0500

    chore: Refactor Dockerfile.php to improve permissions handling and update npm commands

--- Dockerfile.php
+++ Dockerfile.php
@@ -13,12 +13,10 @@ FROM node:14-alpine AS node_builder
 RUN mkdir -p /var/www/html
 WORKDIR /var/www/html
 
-COPY . ./
+COPY ./ ./
 
 # Installs all node packages
 RUN npm install 
-# 
-RUN npm run dev
 # Generating static into /var/www/html
 RUN npm run prod
 
@@ -36,9 +34,9 @@ RUN docker-php-ext-install pdo pdo_pgsql
 
 # Install nodejs 14 from unofficial repo instead of
 # This will not work RUN apk add --no-cache nodejs npm
-# RUN wget https://unofficial-builds.nodejs.org/download/release/v14.4.0/node-v14.4.0-linux-x64-musl.tar.xz -P /opt/
-# RUN tar -xf /opt/node-v14.4.0-linux-x64-musl.tar.xz -C /opt/
-# ENV PATH="$PATH:/opt/node-v14.4.0-linux-x64-musl/bin"
+RUN wget https://unofficial-builds.nodejs.org/download/release/v14.4.0/node-v14.4.0-linux-x64-musl.tar.xz -P /opt/
+RUN tar -xf /opt/node-v14.4.0-linux-x64-musl.tar.xz -C /opt/
+ENV PATH="$PATH:/opt/node-v14.4.0-linux-x64-musl/bin"
 
 # RUN php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/bin/ --filename=composer
 COPY --from=composer /usr/bin/composer /usr/bin/composer
@@ -48,12 +46,14 @@ COPY . ./
 
 RUN composer install --no-interaction --no-progress --no-suggest --no-scripts --no-dev --no-autoloader
 
-COPY --from=node_builder /var/www/html/public /var/www/html/public
+# COPY --from=node_builder /var/www/html/public /var/www/html/public
 # COPY --from=composer /var/www/html/vendor /var/www/html/vendor
 
 # RUN chown -R www-data:www-data /var/www/html
 
 CMD bash -c "composer install && \
+    npm install && \
+    npm run prod && \
     php artisan key:generate && \
     php artisan storage:link && \
     chmod 775 -R /var/www/html/storage/ && \
