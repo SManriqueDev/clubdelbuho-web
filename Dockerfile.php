75be3a56a9cfbb27cc6630c9dc793d904cc06bab
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 19:38:37 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,17 +1,26 @@
-
 FROM php:7.4-fpm-alpine
 
+# Add laravel user and group
 RUN addgroup -g 1000 laravel && adduser -G laravel -g laravel -s /bin/sh -D laravel
 
-RUN mkdir -p /var/www/html
+# Create necessary directories and set permissions
+RUN mkdir -p /var/www/html \
+    && chown -R laravel:laravel /var/www/html
 
-RUN chown -R laravel:laravel /var/www/html
-
+# Set working directory
 WORKDIR /var/www/html
 
-RUN docker-php-ext-install pdo pdo_mysql 
+# Install PHP extensions
+RUN docker-php-ext-install pdo pdo_mysql
 
+# Copy Composer from the official Composer image
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Copy application files
 COPY . .
+
+# Set permissions for application files
+RUN chown -R laravel:laravel /var/www/html
 
 # Expose port 9000 and start php-fpm server
 EXPOSE 9000
