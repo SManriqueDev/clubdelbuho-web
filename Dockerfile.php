ccd49d272d1921316fc46bb952fbbbbb7495e252
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2024-08-03 19:51:05 -0500

    fix: compose

--- Dockerfile.php
+++ Dockerfile.php
@@ -2,6 +2,7 @@ FROM php:7.4-fpm-alpine
 
 ENV COMPOSER_ALLOW_SUPERUSER 1
 
+COPY composer.json composer.lock ./
 # Add laravel user and group
 RUN addgroup -g 1000 laravel && adduser -G laravel -g laravel -s /bin/sh -D laravel
 
